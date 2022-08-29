2fe47a4ca20b264a7624fa930a66e525dc4f06b2
bethel-m <bethelcletus87@gmail.com>
2022-08-29 19:15:09 +0100

    Blog API setup

---
+++ accounts/admin.py
@@ -0,0 +1,21 @@
+import imp
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomeUserChangeForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomeUserChangeForm
+    model = CustomUser
+    list_display = [
+        "email",
+        "username", 
+        "name",
+        "is_staff",
+    ]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("name",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None,{"fields":("name",)}),)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- django_project/settings.py
+++ django_project/settings.py
@@ -37,11 +37,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #local
+    'accounts',
+    'posts',
+
+    #3rd party
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,3 +130,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# custom user model setting
+AUTH_USER_MODEL = "accounts.CustomUser"
+
+# CORS_HEADER settings
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:3000",
+    "http://localhost:8000",
+)
+
+#csrf settings
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
